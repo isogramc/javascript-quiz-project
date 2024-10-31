dbdc1a754a7d1f940b1d57041b585ee0da3e21b0
theresajvrensburg <theresa@isogramconsulting.com>
2024-10-31 22:28:44 +0100

    first iteration

--- src/question.js
+++ src/question.js
@@ -1,7 +1,20 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
-
-    // 2. shuffleChoices()
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = this.shuffleChoices(choices);
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+  // 2. shuffleChoices()
+  shuffleChoices(choices){
+    let shuffled = choices
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+   
+    console.log(choices);
+  }
 }
